f568efa9491a8f97e62fda331cf78ce3bc316b82
Ivo Edgar <ivo.edgy@gmail.com>
2021-09-30 13:37:24 -0300

    Manual letters and not filled heart beating

--- main.ts
+++ main.ts
@@ -5,7 +5,14 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(100)
         basic.showIcon(IconNames.SmallHeart)
         basic.pause(100)
     }
@@ -13,17 +20,23 @@ input.onGesture(Gesture.Shake, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
+        # # # # #
         . # # # .
-        . . # . .
-        . . # . .
-        . . # . .
         . # # # .
+        . # # # .
+        # # # # #
         `)
     basic.pause(1000)
     basic.showIcon(IconNames.Heart)
     basic.pause(500)
     basic.clearScreen()
-    basic.showString("You")
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        # # # # #
+        . # # # .
+        `)
     basic.pause(500)
     basic.clearScreen()
 })
